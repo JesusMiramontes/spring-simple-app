09ddb218caa95e8afe44c675c2bfdaf876c61891
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-06-01 00:42:31 -0500

    Setting the rout

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -2,12 +2,14 @@ package com.example.demo.student;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "api/v1/student") // Set route
 public class StudentController {
 
     @GetMapping
