f3f3f9bc4bfab4d91a2f4de3f12ae67143ab1215
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-06-01 01:40:47 -0500

    Convert a class into a mapping for db table

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,9 +1,25 @@
 package com.example.demo.student;
 
-
+import javax.persistence.*;
 import java.time.LocalDate;
 
+// map this class to db
+@Entity // for hibernate
+@Table // for the table in db
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+
     private Long id;
     private String name;
     private String email;
